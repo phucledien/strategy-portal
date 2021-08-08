4dbbb4d83b4cd437eda6f72ce9d82dd6010c1bdb
phucledien <phucledien@gmail.com>
2021-08-08 14:12:45 +0700

    Feat: add vault boo-ftm, ice-ftm

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,18 @@ export default function Home() {
       address: "0x274c7eC8238dC554c4300807ca49f140a16B42c6",
       lpAddress: "0x2A651563C9d3Af67aE0388a5c8F89b867038089e",
     },
+    {
+      name: "FTM-BOO Spooky Pod",
+      address: "0x0f71013b113cF05D8aE5e5A551e28464deCE3097",
+      lpAddress: "0xec7178f4c41f346b2721907f5cf7628e388a7a58",
+    },
+    {
+      name: "FTM-ICE Spooky Pod",
+      address: "0xA657D27b1695dfb62DB526FB07f1c57D47721c43",
+      lpAddress: "0x623ee4a7f290d11c11315994db70fb148b13021d",
+    },
   ];
+
   return (
     <UseWalletProvider chainId={250}>
       <div class="container mx-auto px-2">
